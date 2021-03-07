db2e021c3345462d6ab5e1bfb43ee192478cfea0
Adam Mištera <amistera@students.zcu.cz>
2021-03-07 14:19:48 +0100

    [/] Updated run script to conda [+] Added logging and model evaluation

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 #PBS -N NER
-#PBS -l select:1:ncpus=1:ngpus=1:gpu_cap=cuda75:mem=16gb:scratch_local=20gb
-#PBS -l walltime=1:00:00
-#PBS -q @adan.grid.cesnet.cz
+#PBS -l select=1:ncpus=1:ngpus=1:gpu_cap=cuda75:mem=16gb:scratch_local=20gb
+#PBS -l walltime=23:59:00
+#PBS -q gpu
 #PBS -m abe
 
 DATADIR=/storage/plzen1/home/amistera
 
 echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $DATADIR/jobs_info.txt
 
-module add python-3.6.2-gcc
+# Load conda
 module add conda-modules-py37
-module add cuda-10.1
+conda activate $DATADIR/conda
 
+# Copy Data
 cp -r $DATADIR/ANLP $SCRATCHDIR || { echo >&2 "Error while copying data!"; exit 1; }
+cd $SCRATCHDIR/ANLP
 
-python3 main.py
+# Run script
+python3 main.py &> log.txt
+cp log.txt $DATADIR/ANLP || { echo >&2 "Error while copying log file!"; exit 3; }
 
+# Clean 
+conda deactivate
 clean_scratch
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+import logging
 import os
 import numpy as np
 import tensorflow as tf
-from keras.layers import Dense, Input
+from keras.layers import Activation, Dense, Input
 from keras.models import Model
 from keras.optimizers import Adam
-from transformers import BertConfig, TFBertModel, BertTokenizer
+from transformers import BertConfig, TFBertModel,BertTokenizer
 
+from label import Label
 from sample import Sample
 
-category_count = 15
 max_length = 16
 configuration = BertConfig()
 save_path = "Model/"
-data_path = "Data/"
 
 if not os.path.exists(save_path):
     os.makedirs(save_path)
@@ -25,12 +25,12 @@ tokenizer = BertTokenizer(save_path + "vocab.txt", strip_accents=False)
 
 
 # Preprocess given dataset
-def preprocess(file, tokenizer):
+def preprocess(tokenizer, file_name):
     samples = []
     categories = set()
     labels = set()
 
-    with open(file, "r", encoding="utf-8") as input_file:
+    with open(file_name, "r", encoding="utf-8") as input_file:
         for line in input_file:
 
             # Lines with comments
@@ -61,19 +61,18 @@ def preprocess(file, tokenizer):
 
 
 # Load saved model or download new one
-def load_model(trainable=True):
+def load_model():
     model = TFBertModel.from_pretrained("bert-base-multilingual-cased")
     model.save_pretrained(save_path)
-    model.trainable = trainable
 
     return model
 
 
 # Train model with given parameters
 # Bert used as the embedding
-# TODO compare sparse_categorical_crossentropy vs categorical_crossentropy
-def train_model(x, y, num_labels, epochs=5, batch_size=64):
-    bert = load_model(False)
+# TODO sparse_categorical_crossentropy vs categorical_crossentropy
+def train_model(x, y, num_labels, epochs=5, batch_size=512):
+    bert = load_model()
     inputs = Input(shape=(max_length,), dtype=tf.int32)
     embedding = bert(inputs)[1]
     outputs = Dense(num_labels, activation='softmax')(embedding)
@@ -86,66 +85,61 @@ def train_model(x, y, num_labels, epochs=5, batch_size=64):
     return model
 
 
-# Trains multiple models for individual parts of label
-def train_multiple_models(x_train, sub_labels):
-    models = []
-
-    for i in range(category_count):
-        y_train = [label[i] for label in sub_labels]
-        models.append(train_model(x_train, y_train, len(y_train)))
-
-    return models
+def test_model():
+    pass
 
 
 # Predict values from model
 def predict(model: Model, x_test, y_test):
-    index = model.predict(x_test)
-    print(y_test[0])
-    print(index)
-
     return model.predict(x_test)
 
 
-# Encodes label to one hot vector
-def encode_label(position, num_labels):
-    encoded = [0] * num_labels
-    encoded[position] = 1
+# Encode label to one hot vector
+def encode_label(label, num_labels):
+    encoded = np.zeros(shape=(1, num_labels))
+    encoded[0][label] = 1
     return encoded
 
 
 # Prepares training data
-def prepare_training_data():
-    samples, categories, labels = preprocess(data_path + "dev.txt", tokenizer)
-    id2label = {i: label for i, label in enumerate(labels)}
-    label2id = dict(zip(id2label.values(), id2label.keys()))
+def prepare_data(filename):
+    samples, categories, labels = preprocess(tokenizer, filename)
+    label2id = {i: label for i, label in enumerate(labels)}
+    id2label = dict(zip(label2id.values(), label2id.keys()))
     num_labels = len(labels)
 
     x = []
     y = []
-    sub_labels = []
 
     for sample in samples:
         encoded_token = tokenizer.encode(sample.token)
-        encoded_label = encode_label(label2id[sample.label.text], num_labels)
+        encoded_label = encode_label(id2label[sample.label.text], num_labels)
         padding_length = max_length - len(encoded_token)
 
         # Add padding to align to max length
         if padding_length > 0:
             encoded_token = encoded_token + ([0] * padding_length)
+            encoded_token = np.array([encoded_token])
 
         x.append(encoded_token)
         y.append(encoded_label)
-        sub_labels.append(sample.label.get_parts())
 
-    return np.array(x), np.array(y), samples, labels, sub_labels, num_labels
+    return x, y, labels, num_labels
 
 
 def main():
-    print("Loading data")
-    x_train, y_train, samples, labels, sub_labels, num_labels = prepare_training_data()
-    print("Training")
-    # model = train_model(x_train, y_train, num_labels)
-    sub_models = train_multiple_models(x_train, sub_labels)
+    logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s', level=logging.INFO)
+    logging.info("Loading data.")
+    x_train, y_train, labels, num_labels = prepare_data("Data/dev.txt")
+    x_test, y_test, test_labels, num_test_labels = prepare_data("Data/test.txt")
+    logging.info("Loading finished.")
+    logging.info(f"Training with {len(x_train)} examples, total {num_labels} labels.")
+    model = train_model(x_train, y_train, num_labels)
+    logging.info("Training finished")
+    logging.info("Evaluate model.")
+    logging.debug(f"Testing with {len(x_test)} examples, total {num_test_labels} labels.")
+    result = model.evaluate(x_test, y_test)    
+    print(dict(zip(model.metrics_names, result)))
 
 
 if __name__ == "__main__":
